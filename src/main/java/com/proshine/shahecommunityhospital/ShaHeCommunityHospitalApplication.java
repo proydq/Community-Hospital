c0af25a326455989b293ea441ad548fe60751f5c
DarkDog2020 <851788565@qq.com>
2025-07-28 10:53:43 +0800

    首次提交

---
+++ src/main/java/com/proshine/shahecommunityhospital/ShaHeCommunityHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.proshine.shahecommunityhospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaHeCommunityHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaHeCommunityHospitalApplication.class, args);
+    }
+
+}
